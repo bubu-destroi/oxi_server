b5ac3698669eba491fb13b4ab3903a6081e795c7
bubu-destroi <zzzarolina@gmail.com>
2024-08-24 13:31:08 +0100

    string typo

--- models/User.model.js
+++ models/User.model.js
@@ -15,7 +15,7 @@ const userSchema = new Schema(
       required: [true, 'address is required'],
     },
     phone_number: {
-      type: Stringt,
+      type: String,
       required: [true, 'Phone number is required'],
     },
     id_card_picture: {
